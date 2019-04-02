6751c02b5d0f4db52db22f1d2c63fc6d050001de
Pavel Nichita <pnichita@ull.edu.es>
2019-04-02 13:49:16 +0100

    -Add systemPort to DeviceCapability

--- src/main/java/matrix/android/app/DeviceCapabilities.java
+++ src/main/java/matrix/android/app/DeviceCapabilities.java
@@ -15,14 +15,16 @@ public class DeviceCapabilities {
         this.desiredCap.setCapability("appActivity", "MainActivity");
         this.desiredCap.setCapability(MobileCapabilityType.UDID, json.getAsJsonObject().get("udid"));
         this.desiredCap.setCapability(MobileCapabilityType.DEVICE_NAME, json.getAsJsonObject().get("name"));
+        this.desiredCap.setCapability("systemPort", json.getAsJsonObject().get("systemPort"));
     }
 
-    public DeviceCapabilities(String device) {
+    public DeviceCapabilities(String device, int systemPort) {
         this.desiredCap.setPlatform(Platform.ANDROID);
         this.desiredCap.setCapability("appPackage","es.ull.hpcg.matrixandroidapp");
         this.desiredCap.setCapability("appActivity", "MainActivity");
         this.desiredCap.setCapability(MobileCapabilityType.UDID, device);
         this.desiredCap.setCapability(MobileCapabilityType.DEVICE_NAME, "default");
+        this.desiredCap.setCapability("systemPort", systemPort);
     }
 
     public DesiredCapabilities getCapabilities() {
