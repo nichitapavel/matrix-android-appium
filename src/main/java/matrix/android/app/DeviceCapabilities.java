f90f33e6e2005b626ee16c8189cc8839fa6c24cc
Pavel Nichita <pnichita@ull.edu.es>
2019-04-05 13:41:08 +0100

    -Less duplicate code for DeviceCapability

--- src/main/java/matrix/android/app/DeviceCapabilities.java
+++ src/main/java/matrix/android/app/DeviceCapabilities.java
@@ -9,22 +9,28 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class DeviceCapabilities {
     DesiredCapabilities desiredCap = new DesiredCapabilities();
 
-    public DeviceCapabilities(JsonElement json) {
+    private void setCommonCapabilities() {
         this.desiredCap.setPlatform(Platform.ANDROID);
         this.desiredCap.setCapability("appPackage","es.ull.hpcg.matrixandroidapp");
         this.desiredCap.setCapability("appActivity", "MainActivity");
-        this.desiredCap.setCapability(MobileCapabilityType.UDID, json.getAsJsonObject().get("udid"));
-        this.desiredCap.setCapability(MobileCapabilityType.DEVICE_NAME, json.getAsJsonObject().get("name"));
-        this.desiredCap.setCapability("systemPort", json.getAsJsonObject().get("systemPort"));
     }
 
-    public DeviceCapabilities(String device, int systemPort) {
-        this.desiredCap.setPlatform(Platform.ANDROID);
-        this.desiredCap.setCapability("appPackage","es.ull.hpcg.matrixandroidapp");
-        this.desiredCap.setCapability("appActivity", "MainActivity");
+    public DeviceCapabilities(JsonElement json) {
+        setCommonCapabilities();
+        this.desiredCap.setCapability(MobileCapabilityType.UDID, json.getAsJsonObject().get("udid"));
+        this.desiredCap.setCapability(MobileCapabilityType.DEVICE_NAME, json.getAsJsonObject().get("name"));
+        if (json.getAsJsonObject().get("systemPort") != null) {
+            this.desiredCap.setCapability("systemPort", json.getAsJsonObject().get("systemPort"));
+        }
+    }
+
+    public DeviceCapabilities(String device, Integer systemPort) {
+        setCommonCapabilities();
         this.desiredCap.setCapability(MobileCapabilityType.UDID, device);
         this.desiredCap.setCapability(MobileCapabilityType.DEVICE_NAME, "default");
-        this.desiredCap.setCapability("systemPort", systemPort);
+        if (systemPort != null){
+            this.desiredCap.setCapability("systemPort", systemPort);
+        }
     }
 
     public DesiredCapabilities getCapabilities() {
